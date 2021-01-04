e6698baa5c1c9098cd9360f1ab1ff6294108632b
Sankalp Jain <sankalp.jain@somaiya.edu>
2021-01-05 00:03:27 +0530

    added Union and Intersection of two sorted arrays with distinct element,if elements are repeating in given array that should be completed don,t know about why auxilary space complexity came O(N+M) and also why auxilary space complexity came O(min(m,n)) for improved approach of these same

---
+++ DSA 450 Questions Love Babbar/Arrays/Find the Union and Intersection of the two sorted arrays.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// Set is implemented as a balanced tree structure that is why it is possible to maintain order between the elements (by specific tree traversal). The time complexity of set operations is O(log n) while for unordered_set, it is O(1).
+// Union
+// void solve()
+// {
+//     int N,M;
+//     cin>>N>>M;
+//     set<int>s;
+// 	for(int i=0; i<N; i++)
+// 	{
+// 	    int input;
+// 	    cin>>input;
+//     	s.insert(input);
+// 	}
+// 	for(int j=0; j<M; j++)
+// 	{
+// 	    int input1;
+// 	    cin>>input1;
+//     	s.insert(input1);
+// 	}
+// 	cout<<s.size()<<endl;
+// }
+
+// Intersection
+// Complexity is linear in the distance between [first1, last1) and [first2, last2): performs up to
+// 2*(count1+count2)-1 comparisons. Where count1 = last1- first1 and count2 = last2- first2
+
+// void solve()
+// {
+    // int N,M;
+    // cin>>N>>M;
+    // int arr[N];
+    // int arr1[M];
+//     vector<int>v(5);
+//     vector<int>::iterator ls,it;
+	// for(int i=0; i<N; i++)
+	// cin>>arr[i];
+	// for(int j=0; j<M; j++)
+	// cin>>arr1[j];
+// 	sort(arr,arr+N);
+// 	sort(arr1,arr1+M);
+// 	ls=set_intersection(arr,arr+N,arr1,arr1+M,v.begin());
+// 	cout<<ls-v.begin()<<endl;
+//   for (it = v.begin(); it != ls; ++it) 
+//   cout << ' ' << *it; 
+//   cout << "\n"; 
+// }
+
+// Time Complexity O(N+M)
+// void solve()
+// {
+//     int N,M;
+//     cin>>N>>M;
+//     int arr[N];
+//     int arr1[M];
+//     for(int i=0; i<N; i++)
+//     cin>>arr[i];
+//     for(int j=0; j<M; j++)
+//     cin>>arr1[j];
+//     int i=0,j=0;
+//     while(i<N && j<M)
+//     {
+//       if(arr[i]<arr1[j])
+//       cout<<arr[i++]<<" ";
+//       else if(arr[i]>arr1[j])
+//       cout<<arr1[j++]<<" ";
+//       else
+//       {
+//         cout<<arr1[j++]<<" ";
+//         i++;
+//       }
+//     }
+//     while(i<N)
+//     cout<<arr[i++]<<" ";
+//     while(j<M)
+//     cout<<arr1[j++]<<" "; 
+// }
+
+// Time Complexity O(N+M)
+void solve()
+{
+    int N,M;
+    cin>>N>>M;
+    int arr[N];
+    int arr1[M];
+    for(int i=0; i<N; i++)
+    cin>>arr[i];
+    for(int j=0; j<M; j++)
+    cin>>arr1[j];
+    int i=0,j=0;
+    while(i<N && j<M)
+    {
+      if(arr[i]<arr1[j])
+      i++;
+      else if(arr[i]>arr1[j])
+      j++;
+      else
+      {
+        cout<<arr1[j++]<<" ";
+        i++;
+      }
+    }
+}
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        solve();
+    }
+	return 0;
+}
