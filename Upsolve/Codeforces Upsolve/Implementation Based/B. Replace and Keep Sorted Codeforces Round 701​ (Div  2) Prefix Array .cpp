4444d4bae79b7981205f77e0b32d44b6e31df48d
Sankalp679 <sankalp.jain@somaiya.edu>
2021-03-05 00:51:29 +0530

    Completed 2 ques 701 codeforces upsolve O(n) and O(1) ASC both

--- Upsolve/Codeforces Upsolve/Implementation Based/B. Replace and Keep Sorted Codeforces Round 701​ (Div  2) Prefix Array .cpp
+++ Upsolve/Codeforces Upsolve/Implementation Based/B. Replace and Keep Sorted Codeforces Round 701​ (Div  2) Prefix Array .cpp
@@ -1,9 +1,9 @@
-// O(N) Approach
+// TC: O(N)
+// ASC: O(2*N)
 
 // #include<bits/stdc++.h>
 // using namespace std;
 // # define ll long long
-
 
 // int main()
 // {
@@ -14,19 +14,9 @@
 //   cin>>x;
 //   vector<ll>dis(n);
 //   vector<ll>dissum(n);
-//   for(ll i=0;i<n;i++)
+//   for(ll i=1;i<n-1;i++)
 //   {
-//     if(n!=1)
-//     {
-//       if(i==0)
-//       {
-//         dis[i]=dis[i+1]-2;
-//       }
-//       if(i!=0 && i!=n-1)
-//       {
-//         dis[i]=dis[i+1]-dis[i-1]-2;
-//       }
-//     }
+//     dis[i]=a[i+1]-a[i-1]-2;
 //   }
 //   dissum[0]=0;
 //   for(ll i=1; i<n-1; i++)
@@ -35,16 +25,39 @@
 //   }
 //   while(q--)
 //   {
-//     ll l,r,ans;
+//     ll l,r,ans=0;
 //     cin>>l>>r;
 //     l--;
 //     r--;
 //     if(l==r)
-//     cout<<k-1<<endl;
+//       cout<<k-1<<endl;
 //     else
 //     {
 //       ans+=a[l+1]-2+k-1-a[r-1];
-//       ans+=dissum[r]-dissum[l-1];
+//       ans+=dissum[r-1]-dissum[l];
+//       cout<<ans<<endl;
 //     }
+//   }
+// }
+
+// TC : O(n+q)
+// ASC : O(const)
+// #include<bits/stdc++.h>
+// using namespace std;
+// # define ll long long
+// int main()
+// {
+//   ll n,q,k;
+//   cin>>n>>q>>k;
+//   vector<ll>a(n);
+//   for(auto &x: a)
+//   cin>>x;
+//   while(q--)
+//   {
+//     ll l,r,ans=0;
+//     cin>>l>>r;
+//     l--;
+//     r--;
+//     cout<<k-a[r]+a[l]-1+2*(a[r]-a[l]-1-(r-l-1))<<endl;
 //   }
 // }
